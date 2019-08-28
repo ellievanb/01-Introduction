28207d1eb26b5c7dd4dce685274f9b6d09ac53c8
Vanbuskirk <ekvanbus@iu.edu>
2019-08-28 18:49:32 -0400

    edits all files and completes assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Elizabeth VanBuskirk (but I go by Ellie).')
+print('My favorite game would have to be The Sims series.')
+print('Although I have some experience in programming, I am concerned with learning some of the new software like Github and any other software we might use in the future.')
+print('I am excited to learn about what goes into building a game and getting the opportunity to create games.')
+print('My stackoverflow.com user number is 11991484.')
+print('The URL to my github.com profile is https://github.com/ellievanb')
